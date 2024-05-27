e28795b189f4bebb07f6ed9cf26a2c5f1012a4e9
Mauricio Prado <u202010482@upc.edu.pe>
2024-05-26 21:18:24 -0500

    feat: added repositories

---
+++ JwtAuthenticationManager/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using JwtAuthenticationManager.Domain.Repositories;
+using JwtAuthenticationManager.Persistence.Context;
+
+namespace JwtAuthenticationManager.Persistence.Repositories;
+
+public class UnitOfWork(SecurityDbContext context): IUnitOfWork
+{
+    private readonly SecurityDbContext _context = context;
+
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
