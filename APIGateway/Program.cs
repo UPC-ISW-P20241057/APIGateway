84530c2d27bd742599a7e2f0037e2a729229806f
Mauricio Prado <u202010482@upc.edu.pe>
2024-05-26 21:42:11 -0500

    added settings on program.cs of gateway

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,11 +1,17 @@
+using JwtAuthenticationManager.Extensions;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddCustomJwtAuthentication();
 
 var app = builder.Build();
+
+await app.UseOcelot();
+
 app.UseHttpsRedirection();
-app.UseOcelot().Wait();
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
