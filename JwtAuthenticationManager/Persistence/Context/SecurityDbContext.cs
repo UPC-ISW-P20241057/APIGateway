c49e7c9903cf0ab8548c022b4a880f0b1d0c1e12
Mauricio Prado <u202010482@upc.edu.pe>
2024-05-26 21:16:35 -0500

    feat: added db context

---
+++ JwtAuthenticationManager/Persistence/Context/SecurityDbContext.cs
@@ -0,0 +1,25 @@
+﻿using JwtAuthenticationManager.Domain.Models;
+using JwtAuthenticationManager.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthenticationManager.Persistence.Context;
+
+public class SecurityDbContext(DbContextOptions options): DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<User>().ToTable("Users");
+        builder.Entity<User>().HasKey(p => p.Id);
+        builder.Entity<User>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<User>().Property(p => p.Email).IsRequired();
+        builder.Entity<User>().Property(p => p.Role).IsRequired();
+        
+        // Apply Snake Case Naming Convention
+        
+        builder.UseSnakeCaseNamingConvention();
+    }
+}
