026adbbb1ae123979e66032dba3a2b08d7d39e8c
Mauricio Prado <u202010482@upc.edu.pe>
2024-05-26 21:14:52 -0500

    feat: added resources

---
+++ JwtAuthenticationManager/Resources/UserResource.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtAuthenticationManager.Resources;
+
+public class UserResource
+{
+    public long Id { get; set; }
+    public string Email { get; set; }
+    public char Role { get; set; }
+    public string Phone { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+}
