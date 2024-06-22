6aa44d7648433097641bb00ea987c312a861fd44
Mauricio Prado <u202010482@upc.edu.pe>
2024-06-22 14:05:37 -0500

    fix: fixed routes because of ocelot restrictions.

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -6,12 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddCustomJwtAuthentication();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
-await app.UseOcelot();
-
 app.UseHttpsRedirection();
-app.UseAuthentication();
+app.UseOcelot().Wait();
 app.UseAuthorization();
+app.MapControllers();
 app.Run();
